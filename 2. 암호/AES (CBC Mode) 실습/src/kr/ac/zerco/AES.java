bebfbeb297430765a34c0f04f6d531b33451492d
choijaeho <dhqkgbs@naver.com>
2022-03-16 18:01:19 +0900

    AES

--- 2. 암호/AES (CBC Mode) 실습/src/kr/ac/zerco/AES.java
+++ 2. 암호/AES (CBC Mode) 실습/src/kr/ac/zerco/AES.java
@@ -8,8 +8,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class AES {
 
-	private static final String key = "aesEncryptionKey";
-	private static final String initVector = "encryptionIntVec";
+	private static final String key = "aesEncryptionKey"; //16Byte == 128bit
+	private static final String initVector = "encryptionIntVec"; //16Byte
 	
 	//인코더 생성
 	private static final Base64.Encoder enc = Base64.getEncoder(); 
@@ -19,14 +19,16 @@ public class AES {
 	
 	public static String encrypt(String value) {
 	    try {
-	        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8")); //초기화백터
-	        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+	        IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8")); // 초기화백터 byte로 변경
+	        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES"); // byte로 변경
 	 
-	        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-	        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+	        //cipher를 만들
+	        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING"); //AES, CBC모드, partial block 채우기
+	        cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv); // mode 
 	 
-	        byte[] encrypted = cipher.doFinal(value.getBytes());
-	        return enc.encodeToString(encrypted);
+	        //실제로 암호화 하는 부분
+	        byte[] encrypted = cipher.doFinal(value.getBytes()); 
+	        return enc.encodeToString(encrypted); //암호문을 base64로 인코딩하여 출력 해줌
 	        
 	    } catch (Exception ex) {
 	        ex.printStackTrace();
@@ -48,15 +50,22 @@ public class AES {
 	    } catch (Exception ex) {
 	        ex.printStackTrace();
 	    }
-	 
 	    return null;
 	}
 	
 	public static void main(String[] args) {
-		String originalString = "password";
+		String originalString = "Plain text는 여러개의 블럭으로 나누어져있다.\r\n" + 
+				"\r\n" + 
+				"블럭이 128비트라고 하면 각각의 블럭으로 암호화 하는 방식에 대해서는 DES, TripleDES, AES를 통해 알 수 있다.\r\n" + 
+				"출처: https://zeroco.tistory.com/63 [zeroco]";
+		
 	    System.out.println("Original String to encrypt - " + originalString);
+	    
+	    //암호화
 	    String encryptedString = encrypt(originalString);
 	    System.out.println("Encrypted String - " + encryptedString);
+
+	    //복호화
 	    String decryptedString = decrypt(encryptedString);
 	    System.out.println("After decryption - " + decryptedString);
 	}
